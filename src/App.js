db39560c23a804d2cf93c2d0cf1f1d8277a95753
Atul <atulfind@gmail.com>
2023-03-25 22:45:36 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [otp, setOtp] = useState("")
+  
+  useEffect(() => {
+
+    if(window.navigator.mozSms) {
+      window.navigator.mozSms.addEventListener('received', function(event) {
+        // Extract the message body
+        const messageBody = event.message.body;
+  
+        // Look for a 6-digit number in the message body
+        const otpRegex = /\b\d{6}\b/;
+        const match = messageBody.match(otpRegex);
+  
+        if (match) {
+          // OTP detected
+          const otp = match[0];
+          console.log(`OTP detected: ${otp}`);
+          setOtp(otp)
+        }
+      });
+    }
+
+    // Listen for incoming SMS messages
+    
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      otp
+      {
+        otp && <div>{otp}</div>
+      }
     </div>
   );
 }
